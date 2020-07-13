7b900affb5034a544cc9ab5b78ae3283eea4f85a
Budsterblue <budsterblue@gmail.com>
2020-07-13 03:10:10 -0500

    final styling fixes

--- main.js
+++ main.js
@@ -8,14 +8,10 @@ document.getElementById("overlaybutton").addEventListener('change', (event) => {
 });
 
 
-// Update Image From URL (Using Button)
-//document.getElementById("updateImage").addEventListener('click', (event) => { overlay.src = document.getElementById("urlSelector").value; });
-
 // Update Image From Local File
 document.getElementById("fileSelector").addEventListener('change', (event) => { overlay.src = window.URL.createObjectURL(document.getElementById("fileSelector").files[0]); });
 
 // Update Image From URL
-// TODO: test this code!
 document.getElementById("urlSelector").addEventListener("keyup", function(event) {
 	event.preventDefault();
 	if (event.keyCode === 13) { overlay.src = document.getElementById("urlSelector").value; }
@@ -29,5 +25,4 @@ document.getElementById("offsetX").addEventListener('change', (event) => { overl
 document.getElementById("offsetY").addEventListener('change', (event) => { overlay.style.top = (document.getElementById("offsetY").value -3240) + "px"; });
 
 // Scale
-//document.getElementById("scaleImg").addEventListener('change', (event) => { overlay.style.width = document.getElementById("scaleImg").value + "px"; });
 document.getElementById("scaleImg").addEventListener('input', (event) => { overlay.style.width = document.getElementById("scaleImg").value + "px"; });
